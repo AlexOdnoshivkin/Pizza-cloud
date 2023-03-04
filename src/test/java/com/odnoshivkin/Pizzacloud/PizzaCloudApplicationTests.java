1766b12c3c408f231e89cd118019614cf5dde8f0
Alex Odnoshivkin <alexander.odnoshivkin@yandex.ru>
2023-03-04 20:43:22 +0300

    feat: add home page

---
+++ src/test/java/com/odnoshivkin/Pizzacloud/PizzaCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odnoshivkin.Pizzacloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
